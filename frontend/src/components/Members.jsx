f86bfb1b77416a04df61af29d4bd861a553f78d3
juddjhf <kumarbairwalucky@gmail.com>
2026-09-13 17:16:37 +0530

    Update Members.jsx

--- frontend/src/components/Members.jsx
+++ frontend/src/components/Members.jsx
@@ -1,19 +1,45 @@
-function Members({ nickname }) {
+
+function Members({ members }) {
+
     return (
         <div className="members">
 
-            <h3>Members</h3>
+            <h3>
+                Members ({members.length})
+            </h3>
 
-            <div className="member">
-                <span className="online-dot"></span>
 
-                <span>
-                    {nickname}
-                </span>
-            </div>
+            {members.length === 0 ? (
+
+                <p>
+                    No members
+                </p>
+
+            ) : (
+
+                members.map((member, index) => (
+
+                    <div
+                        className="member"
+                        key={`${member.nickname}-${index}`}
+                    >
+
+                        <span className="online-dot"></span>
+
+                        <span>
+                            {member.nickname}
+                        </span>
+
+                    </div>
+
+                ))
+
+            )}
 
         </div>
     );
+
 }
 
 export default Members;
+
